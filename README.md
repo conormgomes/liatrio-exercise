74f46ee6336d0205077b756b27c6f138586f9696
conormgomes <gomesconor@gmail.com>
2024-04-17 14:15:04 -0700

    but I actually added the files this time

--- README.md
+++ README.md
@@ -15,3 +15,8 @@ Further setting up repo:
 
 Installed Node.js and began teaching myself javascript and Node specifically via YouTube videos
 
+
+**4/17/24**
+
+Turns out I had enough info from the first YouTube video I watched to do what I think I need to in js. My initial attempts worked, but were a bit janky. I was writing the current time to a json file which was then sent as a response. I realized not too long after that Express has the option to just send a json directly. Also fixed my timestamp to just use Date.now() which I'm hopeful is the correct formatting. Will find out when I get to testing.
+
---
+++ docker-container/src/index.js
@@ -0,0 +1,33 @@
+
+const express = require('express');
+//const {readFile} = require('fs').promises;
+//const {writeFile} = require('fs');
+
+const app = express();
+
+/*
+const path = './msg.json';
+var datetime = new Date(); //Date.now()?
+//var jsonDate = datetime.toJSON();
+//const vals = {message: "My name is Conor", timestamp: new Date(jsonDate).toUTCString()};
+const vals = {message: "My name is Conor", timestamp: datetime};
+*/
+
+/*
+writeFile(path, JSON.stringify(vals, null, 2), (error) => {
+    if(error){
+        console.log('ERROR: ', error);
+        return;
+    }
+
+    console.log('JSON Written Successfully.');
+})
+*/
+
+app.get('/', async (request, response) => {
+
+    //response.send(await readFile('./msg.json', 'utf8'));
+    response.json({message: "My name is Conor", timestamp: Date.now()});
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
