6a6127c877b2a8a2989f8ca6e65f2300a3a192e3
conormgomes <gomesconor@gmail.com>
2024-04-23 16:20:12 -0700

    Addest new Docker push 'latest' and new variable in the json

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', async (request, response) => {
 
-    response.json({message: "My name is Conor", timestamp: Date.now()});
+    response.json({message: "My name is Conor", timestamp: Date.now(), foo: "bar"});
 });
 
 app.listen(80, () => console.log('App available on http://localhost:80'));
