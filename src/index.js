1324f916ab49916e927d2d6ebde2fdba6eaa9f3a
conormgomes <gomesconor@gmail.com>
2024-05-01 10:56:05 -0700

    Live demo commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ app.get('/', async (request, response) => {
 
     response.json({ message: "My name is Conor", 
                     timestamp: Date.now(), 
-                    please: "Work"});
+                    team_name: "Fighting Mongooses",
+                    please: "Hire me :^)"});
 });
 
 app.listen(80, () => console.log('App available on http://localhost:80'));
