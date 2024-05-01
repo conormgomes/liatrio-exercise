6879e083c68731b5d66f3b491f53e3019d42dc0f
conormgomes <gomesconor@gmail.com>
2024-05-01 10:52:32 -0700

    Slight change to json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
-
 const app = express();
 
 app.get('/', async (request, response) => {
 
-    response.json({message: "My name is Conor", timestamp: Date.now(), team_name: "The Fighting Mongooses", please: "Work"});
+    response.json({ message: "My name is Conor", 
+                    timestamp: Date.now(), 
+                    please: "Work"});
 });
 
 app.listen(80, () => console.log('App available on http://localhost:80'));
